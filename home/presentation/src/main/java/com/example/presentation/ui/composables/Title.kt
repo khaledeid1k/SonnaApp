7706bf3ce31c294d0f203e42daa4c24d8ffa24f4
khaledeid1k <khaled.eid1k1k@gmail.com>
2024-01-09 15:05:57 +0200

    make title composable

---
+++ home/presentation/src/main/java/com/example/presentation/ui/composables/Title.kt
@@ -0,0 +1,17 @@
+package com.example.presentation.ui.composables
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun Title(modifier: Modifier=Modifier,text:String,textsStyle: TextStyle=MaterialTheme.typography.titleMedium) {
+    Text(
+        modifier=modifier,
+        text =text,
+        style = textsStyle)
+
+
+}
