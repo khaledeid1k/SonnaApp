eb4c4a18c50865db91c16b78fbc2d23840632b0b
khaledeid1k <khaled.eid1k1k@gmail.com>
2024-01-09 15:03:50 +0200

    add module of design system

---
+++ designsystem/src/main/java/com/example/designsystem/theme/Dimens.kt
@@ -0,0 +1,19 @@
+package com.example.designsystem.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+//region Text Size Dimensions
+val TextSize20 = 20.sp
+val TextSize16 = 16.sp
+//endregion
+
+// region Radius Dimensions
+val Radius23 = 23.dp
+//endregion
+
+
+//region Space Dimensions
+val Spacing16 = 16.dp
+//endregion
+
