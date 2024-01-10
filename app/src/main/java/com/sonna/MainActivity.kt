63725eecff6690f7b7fb735924cfb07bb711204f
khaledeid1k <khaled.eid1k1k@gmail.com>
2024-01-11 00:47:27 +0200

    refactoring main activity

--- app/src/main/java/com/sonna/MainActivity.kt
+++ app/src/main/java/com/sonna/MainActivity.kt
@@ -10,19 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
+import com.example.designsystem.ConfigData
+import com.example.designsystem.theme.SonnaAppTheme
 import com.giraffe.ui.ContentScreen
-import com.giraffe.viewmodel.ContentViewModel
-import com.sonna.ui.theme.SonnaAppTheme
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-//    @Inject
-//    lateinit var contentViewModel: ContentViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
