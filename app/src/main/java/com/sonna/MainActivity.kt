f3e5acd3dc529e6ef8d0c614b615255b9860f287
khaledeid1k <khaled.eid1k1k@gmail.com>
2024-01-11 06:56:46 +0200

    fix imports od main activity

--- app/src/main/java/com/sonna/MainActivity.kt
+++ app/src/main/java/com/sonna/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.designsystem.theme.SonnaAppTheme
-import com.giraffe.ui.ContentScreen
+import com.sonna.presentation.content.ContentScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
