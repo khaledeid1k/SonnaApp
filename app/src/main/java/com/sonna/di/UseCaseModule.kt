e4e57de3c18c1f2b4ebed5581b2b8f49176615f2
khaledeid1k <khaled.eid1k1k@gmail.com>
2024-01-11 07:31:39 +0200

    add get last hadith use case to di

--- app/src/main/java/com/sonna/di/UseCaseModule.kt
+++ app/src/main/java/com/sonna/di/UseCaseModule.kt
@@ -1,6 +1,7 @@
 package com.sonna.di
 
 import com.sonna.domain.repository.ContentRepository
+import com.sonna.domain.usecase.GetLastHadith
 import com.sonna.domain.usecase.GetSurahesUseCase
 import com.sonna.domain.usecase.InsertSurahUseCase
 import dagger.Module
@@ -13,12 +14,17 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object UseCaseModule {
     @Provides
-    fun provideGetSurahesUseCase(contentRepository: com.sonna.domain.repository.ContentRepository): com.sonna.domain.usecase.GetSurahesUseCase {
-        return com.sonna.domain.usecase.GetSurahesUseCase(contentRepository)
+    fun provideGetSurahesUseCase(contentRepository: ContentRepository): GetSurahesUseCase {
+        return GetSurahesUseCase(contentRepository)
     }
 
     @Provides
-    fun provideInsertSurahUseCase(contentRepository: com.sonna.domain.repository.ContentRepository): com.sonna.domain.usecase.InsertSurahUseCase {
-        return com.sonna.domain.usecase.InsertSurahUseCase(contentRepository)
+    fun provideInsertSurahUseCase(contentRepository: ContentRepository): InsertSurahUseCase {
+        return InsertSurahUseCase(contentRepository)
+    }
+
+    @Provides
+    fun provideLAstHadithUseCase(contentRepository: ContentRepository): GetLastHadith {
+        return GetLastHadith(contentRepository)
     }
 }
