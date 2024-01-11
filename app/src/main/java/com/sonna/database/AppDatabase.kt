c0ed85ad6bd038c9824143909c1ee3c87ce3dc4c
khaledeid1k <khaled.eid1k1k@gmail.com>
2024-01-11 06:56:14 +0200

    fix imports od data base base class

--- app/src/main/java/com/sonna/database/AppDatabase.kt
+++ app/src/main/java/com/sonna/database/AppDatabase.kt
@@ -2,10 +2,8 @@ package com.sonna.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.giraffe.data.local.ContentDao
-import com.giraffe.data.local.model.SurahModel
 
-@Database(entities = [SurahModel::class], version = 1)
+@Database(entities = [com.sonna.data.local.model.SurahModel::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun getContentDao(): ContentDao
+    abstract fun getContentDao(): com.sonna.data.local.ContentDao
 }
