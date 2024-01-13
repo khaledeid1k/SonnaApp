90f02e783c7f65c051bb0a571342323e7c237553
khaledeid1k <khaled.eid1k1k@gmail.com>
2024-01-13 19:39:29 +0200

    add home state

--- core/presentation/ui/src/main/java/com/sonna/screens/home/HomeState.kt
+++ core/presentation/ui/src/main/java/com/sonna/screens/home/HomeState.kt
@@ -1,21 +1,26 @@
-package com.sonna.presentation.home
+package com.sonna.screens.home
 
 import androidx.annotation.StringRes
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.sonna.common.R
+import com.sonna.common.theme.darkBlueWithRedTint
+import com.sonna.common.theme.darkGreenishOrange
+import com.sonna.common.theme.greenishYellow
+import com.sonna.common.theme.semiTransparentBlue
+
 
 data class HomeState(
     val categories: List<Category> = listOf(
         Category(height = 163.dp, image = R.drawable.quarn,
-            color = Color(0x4D2BFF00), title = R.string.quran),
+            color = semiTransparentBlue, title = R.string.quran),
         Category(height = 191.dp, image = R.drawable.hadith,
-            color = Color(0xB3FF643D), title = R.string.hadith),
+            color = greenishYellow, title = R.string.hadith),
         Category(height = 191.dp, image = R.drawable.shek,
-            color = Color(0xB3FFB92C), title = R.string.listening),
+            color = darkGreenishOrange, title = R.string.listening),
         Category(height = 163.dp, image = R.drawable.zekr,
-            color = Color(0xB34865FF), title = R.string.azkar),
+            color = darkBlueWithRedTint, title = R.string.azkar),
     ) ,
 
 )
