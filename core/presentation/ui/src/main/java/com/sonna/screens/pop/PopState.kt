b47c55b3178ac2f90c7082295f2da90f26ca73b9
khaledeid1k <khaled.eid1k1k@gmail.com>
2024-01-13 19:24:38 +0200

    add pop state

---
+++ core/presentation/ui/src/main/java/com/sonna/screens/pop/PopState.kt
@@ -0,0 +1,8 @@
+package com.sonna.screens.pop
+
+data class PopState(
+    val quran: String = "",
+    val tafser: String = "",
+    val nameOfSura: String = "",
+    val numberOfAya: String = "",
+)
