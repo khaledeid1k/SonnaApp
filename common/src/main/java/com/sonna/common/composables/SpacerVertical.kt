34edbde8c994dbf7f6b3de8d680f4b450b3aabf2
khaledeid1k <khaled.eid1k1k@gmail.com>
2024-01-14 12:02:56 +0200

    make common Composable spacer

--- common/src/main/java/com/sonna/common/composables/SpacerVertical.kt
+++ common/src/main/java/com/sonna/common/composables/SpacerVertical.kt
@@ -1,17 +1,16 @@
 package com.sonna.common.composables
 
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-
 @Composable
-fun SpacerHorizontal(spaceSize: Dp =0.dp) {
-    Spacer(modifier = Modifier.width(spaceSize))
+fun SpacerVertical(spaceSize: Dp =0.dp) {
+    Spacer(modifier = Modifier.height(spaceSize))
 
 
 }
