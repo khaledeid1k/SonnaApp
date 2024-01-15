7b56c111bc4eb77d551648b5b2e32f78534fcb50
khaledeid1k <khaled.eid1k1k@gmail.com>
2024-01-15 14:34:32 +0200

    add messenger and auth modules

---
+++ auth/domain/src/main/java/com/sonna/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.sonna.domain
+
+class MyClass {
+}
--- messenger/domain/build.gradle.kts
+++ messenger/domain/build.gradle.kts
@@ -1,27 +1,10 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinAndroid)
+    id("java-library")
+    alias(libs.plugins.kotlin)
 }
 
-android {
-    namespace = "com.sonna.domain"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
 }
