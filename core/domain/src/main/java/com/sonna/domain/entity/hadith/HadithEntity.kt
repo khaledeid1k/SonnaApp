daeb996975c0c03b99a03cbc4edd79854aa7a83e
khaledeid1k <khaled.eid1k1k@gmail.com>
2024-02-13 09:42:57 +0200

    add search variable to hadith entity

--- core/domain/src/main/java/com/sonna/domain/entity/hadith/HadithEntity.kt
+++ core/domain/src/main/java/com/sonna/domain/entity/hadith/HadithEntity.kt
@@ -3,5 +3,6 @@ package com.sonna.domain.entity.hadith
 data class HadithEntity(
     val hadith: String,
     val description: String,
-    val number: Int
+    val number: Int,
+    val searchTerm: String,
 )
